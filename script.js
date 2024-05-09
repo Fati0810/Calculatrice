072b574966497cfd9f61f2e9a9e8bef618d7c1af
Fatimata Diallo <fatimatadiallo8@homail.com>
2024-05-09 19:20:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// calc_array = new Array();
+// var calcul = 0;
+// var pas_ch = 0;
+
+// function $id(id){ document.getElementById(id);
+// }
+
+// function f_calc(id,n){
+//     if(n=='ac'){
+//         initialiser_calc(id);
+//     }
+//     else if(n=='='){
+//         if(calc_array[id][0])
+//     }
+// }
+
